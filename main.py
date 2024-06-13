02fe816fb1b0f0b3ae8db8fc3841ac0d5502d9ef
krishnav22 <147203302+krishnav22@users.noreply.github.com>
2024-06-13 23:22:34 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+class Solution:
+    def minMovesToSeat(self, seats, students):
+        seats.sort()
+        students.sort()
+        return sum(abs(seat - student) for seat, student in zip(seats, students))
+        
